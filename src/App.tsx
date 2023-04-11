aeebe08298c4ad7a5d57df78bac5637db81ff76d
Kevin <patter001@gmail.com>
2023-04-10 19:21:53 -0500

    size experiments

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
     const packeryImage = new URL("./images/packery.jpg", import.meta.url)
     const portAImage = new URL("./images/portA.jpg", import.meta.url)
     return (
-        <div style={{display: "flex"}}>
+        <div style={{display: "flex", width: "800px"}}>
             <div style={{ backgroundImage:`url(${packeryImage})`, width: "50%", height:"600px", }}>
                 <div style={{marginLeft: "auto"}}>
                     <WindGaugePackery/>
