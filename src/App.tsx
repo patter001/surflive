b169e59e70bf00515de1bfc847ed3c2cd5b12cc0
Kevin Patterson <patter001@gmail.com>
2024-07-04 10:30:12 -0500

    bigger wave table

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,36 @@ import * as React from "react";
 import {useState, useMemo, useCallback} from "react"
 import { WaveInfo42020 } from "./components/SwellFromBuoy"
 import { WindGaugePackery, WindGaugePortA } from "./components/WindGauge";
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 import {
     QueryClient,
     QueryClientProvider,
   } from '@tanstack/react-query';
-import {WaveChartExample } from "./components/AreaChartExample"
+
+import "./App.css";
+
 const queryClient = new QueryClient()
 
-const squareStyle = {backgroundSize: "cover", backgroundRepeat: "no-repeat", width: "100%", height:"33%", flexGrow: 1 }
+const squareStyle = {backgroundSize: "cover", backgroundRepeat: "no-repeat", width: "100%", height:"50%", flexGrow: 1 }
 
 export function MobileApp () {
     const packeryImage = new URL("./images/packery.jpg", import.meta.url)
     const portAImage = new URL("./images/portA.jpg", import.meta.url)
     return (
         <div style={{ justifyContent: "space-around", width: "100%", height: "100vh"}}>
-            <div style={{ backgroundImage:`url(${packeryImage})`, ... squareStyle}}>
-                <div style={{marginLeft: "auto"}}>
-                    <WindGaugePackery/>
-                </div> 
-            </div> 
-            <div style={{ backgroundImage:`url(${portAImage})`, ... squareStyle}}>
-                <div style={{marginLeft: "auto"}}>
-                <WindGaugePortA/>
-                </div> 
-            </div> 
             <div style={squareStyle}>
-                <WaveInfo42020 count={3}/>
+                <div style={{ backgroundImage:`url(${packeryImage})`, ... squareStyle}}>
+                    <div style={{marginLeft: "auto"}}>
+                        <WindGaugePackery/>
+                    </div> 
+                </div> 
+                <div style={{ backgroundImage:`url(${portAImage})`, ... squareStyle}}>
+                    <div style={{marginLeft: "auto"}}>
+                    <WindGaugePortA/>
+                    </div> 
+                </div> 
+            </div>
+            <div style={squareStyle}>
+                <WaveInfo42020 count={4}/>
             </div>
         </div>        
     )   
