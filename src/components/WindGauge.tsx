6059d8f7b285c32338019288514cc413972bb6b9
Kevin Patterson <patter001@gmail.com>
2024-07-04 10:54:34 -0500

    font size experiments

--- src/components/WindGauge.tsx
+++ src/components/WindGauge.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, ReactElement} from "react"
+import React, { CSSProperties, ReactElement } from "react"
 import "@fortawesome/fontawesome-free/css/all.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Wind, usePackaryWind, usePortAWind } from "../queries/WindQueries";
@@ -7,46 +7,45 @@ import { Wind, usePackaryWind, usePortAWind } from "../queries/WindQueries";
 const arrowColor = "blue";
 
 
-export function WindGaugePackery(){
+export function WindGaugePackery() {
 
     const wind = usePackaryWind();
 
-    if(wind.error){
+    if (wind.error) {
         return (<div color={"white"}>Error fetching wind data</div>)
-    } else if(wind.isLoading){
-        return(<div color={"white"}>Loading..</div>)
-    } else if (wind.data){
-        return(<WindGauge data={wind.data}/>);
+    } else if (wind.isLoading) {
+        return (<div color={"white"}>Loading..</div>)
+    } else if (wind.data) {
+        return (<WindGauge data={wind.data} />);
     } else {
-        return(<div color={"white"}>Unknown..</div>)
+        return (<div color={"white"}>Unknown..</div>)
     }
 }
 
-export function WindGaugePortA(){
+export function WindGaugePortA() {
 
     const wind = usePortAWind();
 
-    if(wind.error){
+    if (wind.error) {
         return (<WindLabel><p>Error fetching data...</p></WindLabel>)
-    } else if(wind.isLoading){
-        return(<WindLabel><p>Loading...</p></WindLabel>);
-    } else if (wind.data){
-        return(<WindGauge data={wind.data}/>);
+    } else if (wind.isLoading) {
+        return (<WindLabel><p>Loading...</p></WindLabel>);
+    } else if (wind.data) {
+        return (<WindGauge data={wind.data} />);
     } else {
-        return(<WindLabel><p>Unknown status...</p></WindLabel>)
+        return (<WindLabel><p>Unknown status...</p></WindLabel>)
     }
 }
 
-function WindArrow({degrees} : {degrees: string}){
-    return <i className={'fas fa-long-arrow-alt-down'} style={{fontSize: "80px", color:arrowColor, transform: `rotate(${degrees}deg)`}}/>
+function WindArrow({ degrees }: { degrees: string }) {
+    return <i className={'fas fa-long-arrow-alt-down'} style={{ fontSize: "55px", color: arrowColor, transform: `rotate(${degrees}deg)` }} />
 }
 
-function WindLabel({children}:{children: ReactElement[] | ReactElement}){
-    const boxStyle: CSSProperties  = {
+function WindLabel({ children }: { children: ReactElement[] | ReactElement }) {
+    const boxStyle: CSSProperties = {
         backgroundColor: "rgba(255,255,255,.3)",
         textAlign: "center",
         color: arrowColor,
-        fontSize: "40px",
     }
     return (
         <div style={boxStyle}>
@@ -57,32 +56,31 @@ function WindLabel({children}:{children: ReactElement[] | ReactElement}){
 
 
 // Displays Data from a NOAA Boy
-export function WindGauge({data} : {data: Wind[]}){
-    const lastEntry = data[data.length-1]
+export function WindGauge({ data }: { data: Wind[] }) {
+    const lastEntry = data[data.length - 1]
     const speedStyle: CSSProperties = {
         color: arrowColor,
-        fontSize: "80px",
+        fontSize: "55px",
         marginLeft: "10px",
         marginRight: "10px",
     }
     const updatedStyle: CSSProperties = {
         color: arrowColor,
-        fontSize: "15px" 
+        fontSize: "15px"
     }
     console.log(lastEntry)
-    const mph = Math.round(Number(lastEntry.s)* 1.150779)
-    const gusts = Math.round(Number(lastEntry.g)* 1.150779)
+    const mph = Math.round(Number(lastEntry.s) * 1.150779)
+    const gusts = Math.round(Number(lastEntry.g) * 1.150779)
     return (
         <WindLabel>
             <div>
                 <div>
-                    <WindArrow degrees={lastEntry.d} />   
-                    <span style={speedStyle}>{mph}-{gusts} mph</span> 
-                    <span style={updatedStyle}>{lastEntry.t}</span> 
+                    <WindArrow degrees={lastEntry.d} />
+                    <span style={speedStyle}>{mph}-{gusts} mph</span>
+                    <br /><span style={updatedStyle}>{lastEntry.t}</span>
                 </div>
 
             </div>
         </WindLabel>
     )
-
 }
